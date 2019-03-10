199143bbc677469931faa57f4ddcfd7fc97d7dff
Daniel Bullock <dnbulloc@iu.edu>
2019-03-09 20:07:26 -0500

    fixed improper reference image pass.  Credit 2 josh for finding

--- CoordTransform.sh
+++ CoordTransform.sh
@@ -73,7 +73,7 @@ echo "$file"
 
 ## non-linear transform
 echo "Converting rois from MNI space to subject space w/ linear and non-linear warp..."
-antsApplyTransforms -d 3 -i $file -r $SUBBRAIN -o output/rois/$basename -t t1_to_mni_1Warp.nii.gz -t [t1_to_mni_0GenericAffine.mat,0] -v
+antsApplyTransforms -d 3 -i $file -r $MNI -o output/rois/$basename -t t1_to_mni_1Warp.nii.gz -t [t1_to_mni_0GenericAffine.mat,0] -v
 
 echo "Done."
 
